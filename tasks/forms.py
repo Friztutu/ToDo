5bde8e995a6151eaaf0f8007aec1f778779cea6d
Alex <erofeevs943@gmail.com>
2023-05-22 06:10:49 +0300

    (ADD) Adding tasks

---
+++ tasks/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from tasks.models import Task
+
+
+class NewTaskForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    deadline = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'id': 'datetime', 'type': 'datetime-local'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'style': 'height: 20em;'}))
+
+    class Meta:
+        model = Task
+        fields = ('title', 'deadline', 'description')
+
+
+class EditTaskForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    deadline = forms.DateTimeField(widget=forms.DateTimeInput())
+    description = forms.Textarea()
+
+    class Meta:
+        model = Task
+        fields = ('title', 'deadline', 'description')
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,8 @@
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView, UpdateView
+from tasks.models import Task
+from django.urls import reverse_lazy
+from tasks.forms import NewTaskForm
 
 
 # Create your views here.
@@ -11,8 +15,15 @@ class ListTasksView(TemplateView):
     template_name = 'tasks/main.html'
 
 
-class NewTaskView(TemplateView):
+class NewTaskView(CreateView):
     template_name = 'tasks/new.html'
+    model = Task
+    success_url = reverse_lazy('tasks:list')
+    form_class = NewTaskForm
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 
 class CardTaskView(TemplateView):
