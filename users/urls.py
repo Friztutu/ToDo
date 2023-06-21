54a9d0d47abfdee2926bd5a74cfaf94a36402a2e
Alex <erofeevs943@gmail.com>
2023-06-20 15:45:49 -1200

    (ADD) Login and Registration users

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
-from users.views import ProfileView, LoginView, RegistrationView
+from users.views import ProfileView, UserLoginView, RegistrationView
 
 app_name = 'users'
 
 
 urlpatterns = [
-    path('profile/', ProfileView.as_view(), name='profile'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
+    path('login/', UserLoginView.as_view(), name='login'),
     path('registration/', RegistrationView.as_view(), name='registration'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,16 +1,42 @@
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.urls import reverse_lazy, reverse
+from django.contrib.auth.views import LoginView
+from django.views.generic import TemplateView
+from django.views.generic.edit import UpdateView, CreateView
+from users.models import CustomUser
+from users.forms import UserLoginForm, RegistrationForm
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    success_url = reverse_lazy('users:profile')
+    form_class = UserLoginForm
+
+    def get_success_url(self):
+        return reverse('users:profile', args=(self.request.user.id,))
+
+
 class ProfileView(TemplateView):
     template_name = 'users/profile.html'
+    model = CustomUser
+
+    def get(self, request, *args, **kwargs):
+        self.kwargs['pk'] = request.user.id
+        return super().get(request, *args, **kwargs)
 
 
-class LoginView(TemplateView):
-    template_name = 'users/login.html'
-
-
-class RegistrationView(TemplateView):
+class RegistrationView(CreateView):
     template_name = 'users/registration.html'
+    model = CustomUser
+    success_url = reverse_lazy('users:login')
+    form_class = RegistrationForm
+
+
+@login_required
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('start_pages:index'))
